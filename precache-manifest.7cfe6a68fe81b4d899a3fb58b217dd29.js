daeaf2f00fd75334497983ae9789eaa4b3f16030
Big 9 <big9@MacBook-Pro.local>
2020-10-04 12:41:58 +0800

    Updates

---
+++ precache-manifest.7cfe6a68fe81b4d899a3fb58b217dd29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13fed0b558728cd3d0108eabd0524639",
+    "url": "/b9downloadapp/index.html"
+  },
+  {
+    "revision": "2f331459cc43819930f3",
+    "url": "/b9downloadapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b03f2dd12b4eb8c2624",
+    "url": "/b9downloadapp/static/js/2.24c5937c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/b9downloadapp/static/js/2.24c5937c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f331459cc43819930f3",
+    "url": "/b9downloadapp/static/js/main.8966a452.chunk.js"
+  },
+  {
+    "revision": "c601ffa8ff804b19e990",
+    "url": "/b9downloadapp/static/js/runtime-main.1b6b7d77.js"
+  },
+  {
+    "revision": "5650db2979ba77579a5b1a3660a451d9",
+    "url": "/b9downloadapp/static/media/big9tv.5650db29.png"
+  },
+  {
+    "revision": "15dd9744fe9ded923cd2bea0629ad7bd",
+    "url": "/b9downloadapp/static/media/downloadbig9image.15dd9744.png"
+  },
+  {
+    "revision": "2c1fab0aea4e054f3d3f1e4b49c2fec1",
+    "url": "/b9downloadapp/static/media/downloadplaystore-logo.2c1fab0a.gif"
+  },
+  {
+    "revision": "8f2dbdb548154fd5e125dc5ba25816a2",
+    "url": "/b9downloadapp/static/media/login-bg.8f2dbdb5.png"
+  }
+]);
